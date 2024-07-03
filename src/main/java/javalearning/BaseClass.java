59d00cf74d213e60a7b1032ebb6bba75b1aaadc0
Prasad <prasad26.te@gmail.com>
2024-07-03 13:59:24 +0530

    InitialCommit

---
+++ src/main/java/javalearning/BaseClass.java
@@ -0,0 +1,12 @@
+package javalearning;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaIntegerPrograms jip=new JavaIntegerPrograms();
+		jip.reverseNumber(123456789);
+		jip.numbersCount(1232694891);
+	}
+
+}
---
+++ src/main/java/javalearning/JavaIntegerPrograms.java
@@ -0,0 +1,31 @@
+package javalearning;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JavaIntegerPrograms {
+
+	public void reverseNumber(int number) {
+		int revNum=0, newNum=0;
+		
+		while(number%10>0) {
+			revNum=revNum*10+number%10;
+			number=number/10;
+		}
+		System.out.println(revNum);
+	}
+
+	public void numbersCount(long number) {
+		char[] charArr=Long.toString(number).toCharArray();
+		Map<Character, Integer> charCount=new HashMap<Character, Integer>();
+		for(int i=0; i<charArr.length;i++) {
+			if(charCount.containsKey(charArr[i])) {
+				charCount.put(charArr[i], charCount.get(charArr[i])+1);
+			}else {
+				charCount.put(charArr[i], 1);
+			}
+		}
+		System.out.println(charCount);
+	}
+
+}
